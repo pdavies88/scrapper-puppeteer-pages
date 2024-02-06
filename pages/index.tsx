b23c51ffcc8c1c60f1791543f54ab116010c76f2
Preston Davies <pdavies88@gmail.com>
2024-02-06 10:53:45 -0500

    useEffect instead of button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Key, useState } from 'react';
+import { Key, useEffect, useState } from 'react';
 
 export default function Home() {
   const [fetchData, setFetchData] = useState([]);
@@ -7,16 +7,21 @@ export default function Home() {
     const data = await res.json();
     setFetchData(data);
   };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <main>
       <h1>Next Pages Router</h1>
       <div>INFO HERE</div>
-      <button
+      {/* <button
         type='button'
         className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
         onClick={getData}>
         Fetch Data
-      </button>
+      </button> */}
       {fetchData &&
         fetchData.map((item: { title: string; votes: string }, i: Key) => (
           <div key={i}>
